c97a5d7f3c1a847923afa182912edb8bc8fa05b9
Igor Tuag <igortuag@gmail.com>
2020-11-18 11:28:06 -0300

    files tree

--- src/components/FlowchartBoard.vue
+++ src/components/FlowchartBoard.vue
@@ -1,21 +1,100 @@
 <template>
-  <div class="board">
-
+  <div>
+    <TreeFolder :folder="tree"/>
   </div>
 </template>
 
 <script>
-export default {
+import TreeFolder from '@/components/TreeFolder.vue';
 
+export default {
+  name: 'FlowchartBoard',
+  data() {
+    return {
+      tree: {
+        id: '1a',
+        name: 'My Computer',
+        children: [
+          {
+            id: '1b',
+            name: 'Documents',
+            children: [
+              {
+                id: '1c',
+                name: 'Personal documents',
+              },
+              {
+                id: '2c',
+                name: 'Work documents',
+              },
+              {
+                id: '3c',
+                name: 'Documents for study',
+              },
+            ],
+          },
+          {
+            id: '2b',
+            name: 'Images',
+            children: [
+              {
+                id: '4c',
+                name: 'Personal images',
+              },
+              {
+                id: '5c',
+                name: 'Work images',
+              },
+              {
+                id: '6c',
+                name: 'Images for study',
+              },
+            ],
+          },
+          {
+            id: '3b',
+            name: 'Musics',
+            children: [
+              {
+                id: '7c',
+                name: 'Personal musics',
+              },
+              {
+                id: '8c',
+                name: 'Work musics',
+              },
+              {
+                id: '9c',
+                name: 'Musics for study',
+              },
+            ],
+          },
+          {
+            id: '4b',
+            name: 'Videos',
+            children: [
+              {
+                id: '10c',
+                name: 'Personal videos',
+              },
+              {
+                id: '11c',
+                name: 'Work videos',
+              },
+              {
+                id: '12c',
+                name: 'Videos for study',
+              },
+            ],
+          },
+        ],
+      },
+    };
+  },
+  components: {
+    TreeFolder,
+  },
 };
 </script>
 
-<style lang="scss" scoped>
-.board {
-  background: #eee;
-  border: 1px solid #222;
-  height: calc(100vh - 6rem);
-  width: 100%;
-  border-radius: 0.5rem;
-}
-</style>
+<style lang="scss" scoped></style>
